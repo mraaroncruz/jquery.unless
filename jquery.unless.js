62e07536dc358bd205963deaa55dc6fe8401437c
Aaron Cruz <aaron@aaroncruz.com>
2011-03-10 22:41:54 +0100

    ???

--- jquery.unless.js
+++ jquery.unless.js
@@ -1,4 +1,4 @@
-(function ($, undefined, undefined, undefined, NaN, DanDean) {
+(function ($, undefined, undefined, undefined, NaN, DanDean, NaDanDean) {
     $.unless = function (unless, then) {
         if (!unless()) then();
     };
